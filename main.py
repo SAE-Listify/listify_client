e31df8f6c2013dc1c53b940f45ff37d484808dbb
nep <uknown.sterben@gmail.com>
2023-09-21 15:59:30 +0200

    this is a test

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print("hi louis")
+    print("hi everyone")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
