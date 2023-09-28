8372db1644077cb321510a6f202b9f82de9a027f
nep <uknown.sterben@gmail.com>
2023-09-28 10:58:23 +0200

    commented and test

--- main.py
+++ main.py
@@ -1,10 +1,122 @@
+import logging
 import sys
-from PyQt5 import QtWidgets, QtCore
 
-app = QtWidgets.QApplication(sys.argv)
-widget = QtWidgets.QWidget()
-widget.resize(400, 200)
-widget.setWindowTitle("Litstify")
-widget.show()
-exit(app.exec_())
-comitted
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QGridLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QComboBox,
+    QMessageBox,
+    QTabWidget,
+    QTextBrowser,
+    QHBoxLayout,
+    QMessageBox,
+)
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QFont, QCloseEvent
+
+
+DEBUG = True
+
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.WARNING)
+
+
+# mainwindow class open tab named listify and size of window is 1000x500
+class MainWindow(QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle("Listify")
+        self.setMinimumSize(1000, 500)
+        self.tab = Tab(self)
+        self.setCentralWidget(self.tab)
+        self.show()
+
+    def closeEvent(self, _e: QCloseEvent):
+        QCoreApplication.exit(0)
+
+#Tab class is the tab widget that holds all the tabs
+class Tab(QWidget):
+    def __init__(self, parent: MainWindow):
+        super(QWidget, self).__init__()
+        self.parent = parent
+        self.layout = QGridLayout(self)
+        self.setLayout(self.layout)
+
+        # Create Tab Widget
+        self.tabWidget = QTabWidget()
+
+        self.tabWidget.setTabsClosable(True)
+        self.tabWidget.setMovable(True)
+        self.tabWidget.tabCloseRequested.connect(self.__closeTab)
+
+        self.tabs = []
+
+        self.layout.addWidget(self.tabWidget)
+
+        # Open test tab
+        self.__openTab("Projet Test")
+        self.__openTab("Projet Test2")
+
+    def __openTab(self, name: str):
+        self.tabs.append(
+            {
+                "widget": QWidget(),
+                "label_test": QLabel("placeholder"),
+                "button_test": QPushButton("placeholder"),
+            }
+        )
+
+        current_tab = self.tabs[-1]
+
+        self.tabWidget.addTab(current_tab["widget"], name)
+
+        # CREATING LAYOUTS
+        current_tab["widget"].layout = QGridLayout()
+
+        # SETTING LAYOUTS TODO is using .layout necessary??
+        current_tab["widget"].setLayout(current_tab["widget"].layout)
+
+        # CREATING WIDGETS
+        # row: int, column: int, rowSpan: int, columnSpan: int
+        ### TESTING
+        current_tab["widget"].layout.addWidget(current_tab["label_test"], 0, 0, 1, 1)
+        current_tab["widget"].layout.addWidget(current_tab["button_test"], 1, 0, 1, 1)
+
+
+
+        # SETTING
+        current_tab["label_test"].setText(f"Test {len(self.tabs)}")
+
+
+    """
+    Executed when a tab is closed by the user, takes the index as its argument
+    """
+    def __closeTab(self, index: int):
+        logging.info(f"Closing Tab index {index}")
+
+        self.tabs.pop(index)
+        self.tabWidget.removeTab(index)
+
+class sidebar(QWidget):
+    def __init__(self, parent: MainWindow):
+        super(QWidget, self).__init__()
+        self.parent = parent
+        self.layout = QGridLayout(self)
+        self.setLayout(self.layout)
+        self.layout.addWidget(QLabel("test"))
+
+class project(QWidget):
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
