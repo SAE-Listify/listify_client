34a943f9e687c205f27c46646869993860db2531
nep <uknown.sterben@gmail.com>
2023-09-28 08:56:59 +0200

    First pyqt5

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-# This is a sample Python script.
+import sys
+from PyQt5 import QtWidgets, QtCore
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("hi everyone")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = QtWidgets.QApplication(sys.argv)
+widget = QtWidgets.QWidget()
+widget.resize(400, 200)
+widget.setWindowTitle("Litstify")
+widget.show()
+exit(app.exec_())
