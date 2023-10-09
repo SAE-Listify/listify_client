123109b11ab7529af465936c5706957a663f49d2
par_defaut <tbilger24@gmail.com>
2023-10-09 13:52:38 +0200

    class projet et fonction de base

--- ui_objects/project.py
+++ ui_objects/project.py
@@ -1,6 +1,14 @@
-import list
+import repository as repo
 
 
 class Project:
-    def _init__(self):
+    def _init__(self, name_project : str = 'Projet', repository_list : list = None): #variable init
+        if repository_list is None: #creation of an empty list if none is given
+            repository_list = []
+        self.__name_project = name_project
+        self.__repository_list = repository_list
         pass
+
+    def __str__(self): #str to print the tittle in the project
+        return f"{self.__name_project}"
+
