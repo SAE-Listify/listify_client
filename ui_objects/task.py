6ed4aa3e6c1f8758bcb4758d5d37f04b8e67d145
jules <c.spinali@spinali-design.fr>
2023-10-16 13:39:02 +0200

    add of delete, change name and get/setter

--- ui_objects/task.py
+++ ui_objects/task.py
@@ -17,7 +17,7 @@ import subtask as sbts
 
 
 class Task(QWidget):
-    def _init__(self, name_task : str = 'Tâche', subtask_list : list = None): #variable init
+    def __init__(self, name_task : str = 'Tache', subtask_list : list = None): #variable init
         super(QWidget, self).__init__()
         if subtask_list is None: #creation of an empty list if none is given
             subtask_list = ['']
@@ -33,3 +33,23 @@ class Task(QWidget):
         self.__task_list[index-1] = sbts.Subtask(f"{name_subtask}") #create the object in the list
     def delete_subtask(self, name_subtask): #delete a subtask using the subtask file
         self.__subtask_list.remove(self.__subtask_list.index(name_subtask.__name.subtask)) #remove the subtask from the list
+
+    def changename_task(self, new_name):
+        self.__name_task = new_name
+
+    @property
+    def name_task(self):
+        return self.__name_task
+
+    @name_task.setter
+    def name_task(self, name_task):
+        self.__name_task = name_task
+
+    @property
+    def subtask_list(self):
+        return self.__subtask_list
+
+    @subtask_list.setter
+    def subtask_list(self, subtask_list):
+        self.__subtask_list = subtask_list
+
