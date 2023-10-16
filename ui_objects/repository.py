d9492d4210e5af8689641aa2268cae8d6fe89476
Titouan-Gacougnolle <97309583+Titouan-Gacougnolle@users.noreply.github.com>
2023-10-16 11:25:44 +0200

    Commit new repo

--- ui_objects/repository.py
+++ ui_objects/repository.py
@@ -21,9 +21,9 @@ from PyQt5.QtWidgets import (
 
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import QFont, QCloseEvent
-class Repository(QWidget):
+class Repository: #(QWidget):
     def __init__(self, name_rep : str = "Repertoire", task_list=None): #variable init
-        super(QWidget,self).__init__()
+        #super(QWidget,self).__init__()
         if task_list is None: #create an empty list if none is given
             task_list = ['']
         self.__name_rep = name_rep
@@ -32,16 +32,17 @@ class Repository(QWidget):
     def __str__(self): #str to print the title in the project
         return f"{self.__name_rep}"
 
-    def create_task(self, name_task): #crate a task with the file task.py
+    def create_task(self, name_task : str = "Tache"): #crawte a task with the file task.py
         self.__task_list.append('') #add a new space in the list
         index = len(self.__task_list) #take the index of the new place
-        self.__task_list[index-1] = ts.Task(f"{name_task}") #create the object in the list
+        self.__task_list[index-1] = ts.Task(name_task) #create the object in the list
 
-    def delete_task(self, name_task):
-        self.__task_list.remove(self.__task_list.index(name_task.__name_task))
+    def delete_task(self, num : int ):
+        del self.__task_list[num]
 
-    def change_name(self, new_name): #change the name
+    def changename_rep(self, new_name): #change the name
         self.__name_rep = new_name
+
 
     @property
     def name_rep(self):
