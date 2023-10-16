5b801a7261c4b4d7541d30d6610cacfc5b613fc6
jules <c.spinali@spinali-design.fr>
2023-10-16 14:49:05 +0200

    Task class done, passed the test

--- ui_objects/task.py
+++ ui_objects/task.py
@@ -1,3 +1,5 @@
+import subtask as sbts
+
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -14,29 +16,26 @@ from PyQt5.QtWidgets import (
     QMessageBox,
 )
 
-import subtask as sbts
 
-class Task(QWidget):
-    def __init__(self, name_task: str = 'Tache', subtask_list: list = None):  # variable init
-        super(QWidget, self).__init__()
-        if subtask_list is None:  # creation of an empty list if none is given
-            subtask_list = ['']
+class Task: #(QWidget):
+    def __init__(self, name_task : str = 'Tache', subtask_list = None): #variable init
+        #super(QWidget, self).__init__()
+        if subtask_list is None: #creation of an empty list if none is given
+            subtask_list = []
         self.__name_task = name_task
         self.__subtask_list = subtask_list
 
-    def __str__(self):  # str to print the tittle in the project
+    def __str__(self): #str to print the tittle in the project
         return f"{self.__name_task}"
 
-    def create_subtask(self, name_subtask: str):  # create a subtask using the subtask file
-        self.__subtask_list.append('')  # add a new space to the list
-        index = len(self.__subtask_list)  # take the index of the new space
-        self.__subtask_list[index - 1] = sbts.Subtask(f"{name_subtask}")  # create the object in the list
+    def create_subtask(self, name_subtask : str = "Sous tache"): #create a subtask using the subtask file
+        self.__subtask_list.append('') #add a new space to the list
+        index = len(self.__subtask_list) #take the index of the new space
+        self.__subtask_list[index-1] = sbts.Subtask(name_subtask) #create the object in the list
+    def delete_subtask(self, num): #delete a subtask using the subtask file
+        del self.__subtask_list[num]
 
-    def delete_subtask(self, name_subtask):  # delete a subtask using the subtask file
-        self.__subtask_list.remove(
-            self.__subtask_list.index(name_subtask.__name.subtask))  # remove the subtask from the list
-
-    def changename_task(self, new_name):
+    def changename_task(self, new_name): #change the name of the task
         self.__name_task = new_name
 
     @property
