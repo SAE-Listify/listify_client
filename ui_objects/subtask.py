5aaf6ee011357851c5384bd89739e9a4349301c5
louis <louistefn@gmail.com>
2023-10-16 09:52:05 +0200

    subtask

--- ui_objects/subtask.py
+++ ui_objects/subtask.py
@@ -1,6 +1,17 @@
-import task
+import subtask as sbts
 
+class Subtask:
 
-class SubTask:
-    def _init__(self):
+    def _init__(self, name_subtask : str = 'Sous-Tâche', subtask_list : list = None): #variable init
+        if subtask_list is None: #creation of an empty list if none is given
+            subtask_list = []
+        self.__name_subtask = name_subtask
+        self.__subtask_list = subtask_list
         pass
+
+    def __str__(self): #str to print the tittle in the project
+        return f"{self.__name_subtask}"
+
+    def create_subtask(self, name_subtask : str): #create a subtask using the subtask file
+        name_subtask = sbts.subtask(f"{name_subtask}")
+        self.__subtask_list.append(name_subtask) #add the subtask to the list
