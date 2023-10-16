e6d4481d2270ea2fced08b36e80193530cc79a81
Titouan-Gacougnolle <97309583+Titouan-Gacougnolle@users.noreply.github.com>
2023-10-16 15:16:19 +0200

    Commit repo clean

--- ui_objects/repository.py
+++ ui_objects/repository.py
@@ -1,8 +1,5 @@
 import task as ts
 
-import logging
-import sys
-
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -19,30 +16,28 @@ from PyQt5.QtWidgets import (
     QMessageBox,
 )
 
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QFont, QCloseEvent
-class Repository: #(QWidget):
-    def __init__(self, name_rep : str = "Repertoire", task_list=None): #variable init
-        #super(QWidget,self).__init__()
-        if task_list is None: #create an empty list if none is given
-            task_list = ['']
+
+class Repository:  # (QWidget):
+    def __init__(self, name_rep: str = "Repertoire", task_list=None):  # variable init
+        # super(QWidget,self).__init__()
+        if task_list is None:  # create an empty list if none is given
+            task_list = []
         self.__name_rep = name_rep
         self.__task_list = task_list
 
-    def __str__(self): #str to print the title in the project
+    def __str__(self):  # str to print the title in the project
         return f"{self.__name_rep}"
 
-    def create_task(self, name_task : str = "Tache"): #crawte a task with the file task.py
-        self.__task_list.append('') #add a new space in the list
-        index = len(self.__task_list) #take the index of the new place
-        self.__task_list[index-1] = ts.Task(name_task) #create the object in the list
+    def create_task(self, name_task: str = "Tache"):  # crawte a task with the file task.py
+        self.__task_list.append('')  # add a new space in the list
+        index = len(self.__task_list)  # take the index of the new place
+        self.__task_list[index - 1] = ts.Task(name_task)  # create the object in the list
 
-    def delete_task(self, num : int ):
+    def delete_task(self, num: int):
         del self.__task_list[num]
 
-    def changename_rep(self, new_name): #change the name
+    def changename_rep(self, new_name):  # change the name
         self.__name_rep = new_name
-
 
     @property
     def name_rep(self):
