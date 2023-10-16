d67518c76551f10ceefd91cc8072f5f858ab4ef7
par_defaut <tbilger24@gmail.com>
2023-10-16 09:16:52 +0200

    oskour je ne sais pas ce que je fais

--- ui_objects/project.py
+++ ui_objects/project.py
@@ -1,14 +1,41 @@
 import repository as repo
 
+import logging
+import sys
 
-class Project:
-    def _init__(self, name_project : str = 'Projet', repository_list : list = None): #variable init
-        if repository_list is None: #creation of an empty list if none is given
-            repository_list = []
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QGridLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QComboBox,
+    QMessageBox,
+    QTabWidget,
+    QTextBrowser,
+    QHBoxLayout,
+    QMessageBox,
+)
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QFont, QCloseEvent
+class Project(QWidget):
+    def _init__(self, name_project : str = 'Project', repository_list : list = None):  # variable init
+        if repository_list is None:  # creation of an empty list if none is given
+            repository_list = ['']
         self.__name_project = name_project
         self.__repository_list = repository_list
-        pass
 
-    def __str__(self): #str to print the tittle in the project
+
+    def __str__(self):  # str to print the tittle in the project
         return f"{self.__name_project}"
+
+    def create_repository(self, name_repository):  # crate a repository with the file task.py
+        self.__repository_list.append('')  # add a new space in the list
+        index = len(self.__repository_list)  # take the index of the new place
+        self.__repository_list[index - 1] = repo.Repository(f"{name_repository}")  # create the object in the list
+
+    def delete_repository(self, name_repository):
+        self.__repository_list.remove(self.__repository_list.index(name_repository.__name_repository))
 
