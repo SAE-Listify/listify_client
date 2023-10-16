dc9a6f8d0c583a1cae97e036c17605698231bd55
par_defaut <tbilger24@gmail.com>
2023-10-16 14:48:12 +0200

    modif cool

--- ui_objects/project.py
+++ ui_objects/project.py
@@ -1,3 +1,4 @@
+
 import repository as repo
 
 import logging
@@ -20,24 +21,25 @@ from PyQt5.QtWidgets import (
 )
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtGui import QFont, QCloseEvent
-class Project(QWidget):
-    def __init__(self, name_project : str = 'Project', repository_list : list = None):  # variable init
-        super(QWidget,self).__init__()
+
+
+class Project: #(QWidget):
+    def __init__(self, name_project: str = 'Project', repository_list: list = None):  # variable init
+        # super(QWidget,self).__init__()
         if repository_list is None:  # creation of an empty list if none is given
             repository_list = []
         self.__name_project = name_project
         self.__repository_list = repository_list
 
-
     def __str__(self):  # str to print the tittle in the project
         return f"{self.__name_project}"
 
-    def create_repository(self, name_repository):  # crate a repository with the file repository.py
+    def create_repository(self, name_repository):  # crate a repository with the file task.py
         self.__repository_list.append('')  # add a new space in the list
         index = len(self.__repository_list)  # take the index of the new place
         self.__repository_list[index - 1] = repo.Repository(f"{name_repository}")  # create the object in the list
 
-    def delete_repository(self, num: int):
+    def delete_repository(self, num: int): # delete the repository from the project
         del self.__repository_list[num]
 
     def changename_proj(self, new_name):  # change the name
