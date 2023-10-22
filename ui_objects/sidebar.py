49f95a4f14d33b64aa8693c6cf2a467ec4f3ea2f
ldsvrn <70917976+ldsvrn@users.noreply.github.com>
2023-10-22 20:48:04 +0200

    Ouverture & création de projets via la sidebar  (#13)
    * delete sidebar sublayout when closing a project
    
    * translations & deleted useless method
    
    * sidebar: load project from main Listify class
    
    * add methods to handle reopening tabs of available projects
    
    * sidebar: disable switching to tab index, only open the tab when project is no opened
    
    * project placeholder: set label to project name
    
    * sidebar: open last tab on project opening & creation

--- ui_objects/sidebar.py
+++ ui_objects/sidebar.py
@@ -6,52 +6,89 @@ class Sidebar(QWidget):
         super(Sidebar, self).__init__(self.parent)
         self.setFixedWidth(150)
         self.__layout = QVBoxLayout(self)
-        self.__projects_buttons = []  # List to store project buttons
-        self.__delete_buttons = []    # List to store delete buttons
-        self.__setup_projects()
 
-    def __setup_projects(self):
+        self.__projects_layouts = []    # List to store QHBoxLayouts
+        self.__projects_open_buttons = []  # List to store project buttons
+        self.__projects_delete_buttons = []    # List to store delete buttons
+
         # Add create button and text box for project name
-        create_button = QPushButton("Create", self)
+        create_button = QPushButton("Créer un projet", self)
         create_button.clicked.connect(self.__create_project)
         self.__layout.addWidget(create_button)
         self.project_name_textbox = QLineEdit(self)
-        self.project_name_textbox.setPlaceholderText("Enter project name")
+        self.project_name_textbox.setPlaceholderText("Nom du projet")
         self.__layout.addWidget(self.project_name_textbox)
 
-    def __create_project(self):
-        # Create new project button and its associated delete button
-        project_name = self.project_name_textbox.text()
-        if not project_name:
-            QMessageBox.warning(self, "Warning!!!", "please enter project name ?")
-            return
+        # Load projects
+        self.__load_projects()
+
+    """
+    This method creates the UI elements present on the sidebar for each project
+    """
+    def __init_project_list_ui_elements(self, project_name):
         project_hbox = QHBoxLayout()
+
+        # Open Button
         project_button = QPushButton(project_name, self)
         project_button.clicked.connect(self.__open_project)
         project_hbox.addWidget(project_button)
+
+        # Delete Button
         delete_button = QPushButton('X', self)
         delete_button.setFixedSize(20, 20)
         delete_button.clicked.connect(self.__delete_project)
         project_hbox.addWidget(delete_button)
-        self.__projects_buttons.append(project_button)
-        self.__delete_buttons.append(delete_button)
+
+        # maybe work on using dict
+        self.__projects_open_buttons.append(project_button)
+        self.__projects_delete_buttons.append(delete_button)
+        self.__projects_layouts.append(project_hbox)
         self.__layout.addLayout(project_hbox)
+
+    """
+    This methods loads projects available in the main Listify class 
+    """
+    def __load_projects(self):
+        for proj in self.parent.projects:
+            self.__init_project_list_ui_elements(proj.name_project)
+
+    """
+    This method create the project, connected to "create_button"
+    """
+    def __create_project(self):
+        # Create new project button and its associated delete button
+        project_name = self.project_name_textbox.text()
+        if not project_name:
+            QMessageBox.warning(self, "Erreur", "Veuillez choisir un nom!")
+            return
+
+        self.__init_project_list_ui_elements(project_name)
+
+        # Create the project using the parent (Listify class in main.py) method
+        self.parent.new_project(project_name)
+        self.parent.goto_last_tab()
 
     def __delete_project(self):
         # Remove the project button and its associated delete button
-        clicked_button = self.sender()
-        index = self.__delete_buttons.index(clicked_button)
-        button_to_remove = self.__projects_buttons.pop(index)
-        delete_button_to_remove = self.__delete_buttons.pop(index)
-        button_to_remove.deleteLater()
+        clicked_button = self.sender()  # Returns the button object of the clicked button
+        index = self.__projects_delete_buttons.index(clicked_button)
+
+        # first pop the objects from the lists, then properly delete them
+        open_button_to_remove = self.__projects_open_buttons.pop(index)
+        delete_button_to_remove = self.__projects_delete_buttons.pop(index)
+        layout_to_remove = self.__projects_layouts.pop(index)
+        open_button_to_remove.deleteLater()
         delete_button_to_remove.deleteLater()
+        layout_to_remove.deleteLater()
 
     def __open_project(self):
         # Switch to the clicked project tab
         clicked_button = self.sender()
-        index = self.__projects_buttons.index(clicked_button)
-        self.__switch_to_project_tab(index)
+        index = self.__projects_open_buttons.index(clicked_button)
+        if not self.parent.projects[index] in self.parent.opened_projects:
+            self.parent.open_tab_by_project_index(index)
+            self.parent.goto_last_tab()
 
     def __switch_to_project_tab(self, index: int):
         # todo make the logic for later, quite complicated gonna need help for this one
-        self.parent.project_tab_widget.setCurrentIndex(index)
+        self.parent.goto_tab_index(index)
