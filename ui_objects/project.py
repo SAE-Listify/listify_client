13c6d5a9326c952d318a4293bc2e684b9d2e7c1b
ldsvrn <louis2555@orange.fr>
2023-12-10 16:03:21 +0100

    fix project layouts

--- ui_objects/project.py
+++ ui_objects/project.py
@@ -24,17 +24,14 @@ class Project(QWidget):
         self.__name_project = name_project
         self.__repository_list = repository_list
 
+        # Creating layouts and widgets
         self.__layout = QVBoxLayout()
         self.__layout .setAlignment(Qt.AlignTop)
         self.setLayout(self.__layout)
 
-        self.__button_layout = QHBoxLayout()
-        self.__button_layout.setAlignment(Qt.AlignTop)
-        self.__layout.addLayout(self.__button_layout)
+        self.__layout_repo = QHBoxLayout()
+        self.__layout_repo.setAlignment(Qt.AlignLeft)
 
-        self.__layoutRepo = QHBoxLayout()
-        self.__layoutRepo.setAlignment(Qt.AlignLeft)
-        self.__layout.addLayout(self.__layoutRepo)
 
         self.__project_label = QLabel(self.__name_project)
 
@@ -42,12 +39,14 @@ class Project(QWidget):
         self.__create_repo_button.clicked.connect(
             lambda: self.create_repository("repo_test")
         )
-        #self.__layout.addWidget(self.__create_repo_button)
-        self.__button_layout.addWidget(self.__create_repo_button)
+
+        # adding the button and the repos layout to the main project layout
+        self.__layout.addWidget(self.__create_repo_button)
+        self.__layout.addLayout(self.__layout_repo)
 
         if self.__repository_list:
             for repo_widget in self.__repository_list:
-                self.__layoutRepo.addWidget(repo_widget)
+                self.__layout_repo.addWidget(repo_widget)
 
     def __str__(self):  # str to print the tittle in the project
         return f"{self.__name_project}"
@@ -55,7 +54,7 @@ class Project(QWidget):
     def create_repository(self, name_repository):  # crate a repository with the file task.py
         repo_created = repo.Repository(f"{name_repository}")
         self.__repository_list.append(repo_created)  # create the object in the list
-        self.__layout.addWidget(repo_created)
+        self.__layout_repo.addWidget(repo_created)
 
     def delete_repository(self, num: int):  # delete the repository from the project
         del self.__repository_list[num]
